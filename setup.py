c49def07124ed7393983d864ccd8e21af870fc03
pruthvivikram <vikrampruthvi5@gmail.com>
2018-06-29 19:58:01 +0530

    setup.py with ijmports

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pycharm-projects',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='pruthvivikram',
+    author_email='vikrampruthvi5@gmail.com',
+    description='first version for test'
+)
