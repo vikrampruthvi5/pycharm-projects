c665f8570c7efe03e76566e204390e47315a5a0f
pruthvivikram <vikrampruthvi5@gmail.com>
2018-06-29 08:48:02 +0530

    Basic operators

---
+++ 01_basics/02_numbers.py
@@ -0,0 +1,23 @@
+
+# Numbers in python
+
+x = 5  # type: int
+y = 2  # type: int
+
+add = x + y  # type: int
+sub = x - y
+mul = x * y
+div = x / y
+mod = x % y
+pow = x ** y
+
+print("add : {0}\nsub : {1}\nmul : {2}\ndiv : {3}\nmod : {4}\npow : {5}\n".format(add, sub, mul, div, mod, pow))
+
+# Special operators
+
+a = 10.5  # type: float
+b = 2  # type: int
+
+reg_div = a/b  # type: float
+rounded_div = a//b  # type: float
+print("div : {0}\nrounded division : {1}".format(reg_div, rounded_div))
