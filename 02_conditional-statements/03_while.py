30005360d7df7d9924fbb9aa026f87c3200bb7e6
vikram pruthvi <vikrampruthvi5@gmail.com>
2018-07-02 22:34:42 +0000

    while

--- 02_conditional-statements/03_while.py
+++ 02_conditional-statements/03_while.py
@@ -1,15 +1,21 @@
 
-x = 0
+# magic_number = 10
+# c=1
+#
+# while c < magic_number:
+#     print(c)
+#     c += 1
+# print("Magic number reached")
 
-while x < 10:
-    print(x)
-    x += 1
 
-c = 20
+# Assignment
+# Print numbers btn 1 - 100 that are multiples of 4
+start = 1
+end = 100
 
-for n in range(c):
-    if n is 10:
-        print("Magic number reached")
-        break
-    else:
-        print(n)
+print("test")
+
+while start <= end:
+    if start % 4 is 0:
+        print (start, "is multiple of 4")
+    start += 1
