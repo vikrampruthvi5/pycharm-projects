9d8ba8ff4c59dc2c70484ba5b153b0e86f88674c
pruthvivikram <vikrampruthvi5@gmail.com>
2018-07-08 13:56:59 +0530

    Pi testing - code worked. GPIO output worked. But reading from photoresistor sensor failed. Need to try with PWM(Pulse with modulation)

--- raspberrypi-python/02_laser_security.py
+++ raspberrypi-python/02_laser_security.py
@@ -1,4 +1,4 @@
-
+from slackclient import SlackClient
 import RPi.GPIO as gpio
 import time
 
@@ -6,11 +6,23 @@ import time
 gpio.setmode(gpio.BOARD)
 
 laser = 38
+pres = 40
 
 gpio.setup(laser, gpio.OUT)
+gpio.setup(pres, gpio.IN)
+
+# Slack Client configuration
+slack_token = 'xoxp-393473516853-392784516193-392646474624-70cbc54843a64c43cd31bfc52fb5ed22'
+sc = SlackClient(slack_token)
+
+# Configure slack message
+message = ""
+channel = 'python-projects'
+call_type = "chat.postMessage"
 
 while True:
-    gpio.output(laser, 1)
-    time.sleep(1)
     gpio.output(laser, 0)
     time.sleep(1)
+    message = ("Photo level in room is : ",gpio.input(pres))
+    sc.api_call(call_type, channel=channel, text=message)
+
--- raspberrypi-python/slack-manage.py
+++ raspberrypi-python/slack-manage.py
@@ -1,8 +1,8 @@
-import os
+
 
 from slackclient import SlackClient
 
-slack_token = 'xoxp-393473516853-392784516193-392646474624-70cbc54843a64c43cd31bfc52fb5ed22'
+slack_token = 'xoxp-393473516853-392784516193-392698899968-0d5405a8e6494dfca7de6835981b447a'
 sc = SlackClient(slack_token)
 
 # Get the data from the user
